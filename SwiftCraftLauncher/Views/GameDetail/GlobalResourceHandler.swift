2019fbcc4de068e7f57b9778ea16c9c42bf34af6
suhang12332 <1552899301@qq.com>
2025-08-23 20:19:31 +0800

    feat-优化版本显示

--- SwiftCraftLauncher/Views/GameDetail/GlobalResourceHandler.swift
+++ SwiftCraftLauncher/Views/GameDetail/GlobalResourceHandler.swift
@@ -520,7 +520,13 @@ struct CommonSheetGameBody: View {
         Picker("global_resource.select_game".localized(), selection: $selectedGame) {
             Text("global_resource.please_select_game".localized()).tag(Optional<GameVersionInfo>(nil))
             ForEach(compatibleGames, id: \ .id) { game in
-                Text("\(game.gameName)-\(game.gameVersion)-\(game.modLoader)-\(game.modVersion)").tag(Optional(game))
+                (
+                    Text("\(game.gameName)-")
+                    + Text("\(game.gameVersion)-").foregroundStyle(.secondary)
+                    + Text("\(game.modLoader)-")
+                    + Text("\(game.modVersion)").foregroundStyle(.secondary)
+                )
+                .tag(Optional(game))
             }
         }
         .pickerStyle(.menu)
