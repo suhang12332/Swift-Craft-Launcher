8958e90a77a58251d4e537ca6ebe5a8f365c87f4
suhang12332 <48873455+suhang12332@users.noreply.github.com>
2026-04-08 23:28:35 +0800

    Update SidebarView.swift

--- SwiftCraftLauncher/MainFeature/Views/Root/SidebarView.swift
+++ SwiftCraftLauncher/MainFeature/Views/Root/SidebarView.swift
@@ -30,7 +30,6 @@ public struct SidebarView: View {
                         HStack(spacing: 6) {
                             Label(type.localizedName, systemImage: type.systemImage)
                         }
-                        .layoutPriority(1)
                     }
                 }
             }
@@ -47,7 +46,6 @@ public struct SidebarView: View {
                             Text(game.gameName)
                                 .lineLimit(1)
                         }
-                        .layoutPriority(1)
                         .tag(game.id)
                     }
                     .contextMenu {
@@ -70,7 +68,6 @@ public struct SidebarView: View {
                         HStack(spacing: 6) {
                             Label(name, systemImage: "exclamationmark.triangle")
                         }
-                        .layoutPriority(1)
                         .contextMenu {
                             Button(role: .destructive) {
                                 gameActionManager.deleteCorruptedGame(
