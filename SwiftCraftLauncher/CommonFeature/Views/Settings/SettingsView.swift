2094a070b22a9e6213a15299d074a064682c1c4a
suhang12332 <48873455+suhang12332@users.noreply.github.com>
2026-08-04 12:23:30 +0800

    feat: 将设置的样式抽离出来
    feat: 将设置的样式抽离出来

--- SwiftCraftLauncher/CommonFeature/Views/Settings/SettingsView.swift
+++ SwiftCraftLauncher/CommonFeature/Views/Settings/SettingsView.swift
@@ -79,36 +79,3 @@ public struct SettingsView: View {
         }
     }
 }
-
-struct CustomLabeledContentStyle: LabeledContentStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        LabeledContent {
-            configuration.content
-        } label: {
-            HStack(spacing: 0) {
-                configuration.label
-                Text(":")
-            }
-        }
-        .padding(.vertical, 2)
-    }
-}
-
-struct CustomLabeledContentStyleNoColon: LabeledContentStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        LabeledContent {
-            configuration.content
-        } label: {
-            configuration.label
-        }
-        .padding(.vertical, 2)
-    }
-}
-
-extension LabeledContentStyle where Self == CustomLabeledContentStyle {
-    static var custom: Self { .init() }
-}
-
-extension LabeledContentStyle where Self == CustomLabeledContentStyleNoColon {
-    static var customNoColon: Self { .init() }
-}
